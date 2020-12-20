fa13a83279f012bd6ebdd3a876e56c950a414976
Sanchit Kalra <sanchit.kalra1@gmail.com>
2020-12-20 19:26:01 +0530

    Day 3 Complete

---
+++ Day1/DataTypes.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class DataTypes {
+    public static void main(String[] args){
+        System.out.println("x");
+    }
+}
+
+
+
+
+
+// import java.io.*;
+// import java.util.*;
+// import java.text.*;
+// import java.math.*;
+// import java.util.regex.*;
+
+// public class DataTypes {
+	
+//     public static void main(String[] args) {
+//         int i = 4;
+//         double d = 4.0;
+//         String s = "HackerRank ";
+		
+//         Scanner scan = new Scanner(System.in);
+
+//         /* Declare second integer, double, and String variables. */
+        
+//         int second = 0;
+//         int second_d = 0.0;
+//         int second_str = "";
+        
+//         /* Read and save an integer, double, and String to your variables.*/
+//         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+//         scan.nextInt();
+//         scan.nextDouble();
+//         scan.nextLine();
+        
+//         /* Print the sum of both integer variables on a new line. */
+        
+//         System.out.println(d+second_d);
+    
+//         /* Print the sum of the double variables on a new line. */
+		
+//         System.out.println(i+second);
+        
+//         /* Concatenate and print the String variables on a new line; 
+//         	the 's' variable above should be printed first. */
+            
+//         System.out.println(s+second_str);
+
+//         scan.close();
+//     }
+// }
