650d61a22f368125561321b69edf295b53344eb1
Sanchit Kalra <sanchit.kalra1@gmail.com>
2020-12-20 21:22:21 +0530

    Day 1 updated

---
+++ Day1/data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+s_i = 0
+s_d = 0.0
+s_s = ""
+# Read and save an integer, double, and String to your variables.
+s_i = int(input())
+s_d = float(input())
+s_s = input()
+# Print the sum of both integer variables on a new line.
+print(i+s_i)
+# Print the sum of the double variables on a new line.
+print(d+s_d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s_s)
